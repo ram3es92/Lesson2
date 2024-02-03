7861c371deecd0b0a6010f8a9775f00a3576479e
Roman_Knyazev <ram3es92@mail.ru>
2024-02-03 10:46:02 +0300

    решили 3 задачу

---
+++ task3/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = 10;
+int[] arr = {2, 3, 5, 3, 2, 7, 6, 2, 8, 9};
+int i = 0;
+
+while (i < arr.Length)
+{
+    if(arr[i] % 2 == 0)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    i = i + 1;
+}
