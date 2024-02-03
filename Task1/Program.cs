edb11d1e58aa07a1d9c08ea58a8906cd1b4c96ba
Roman_Knyazev <ram3es92@mail.ru>
2024-02-03 10:13:52 +0300

    Добавили массивы в код

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] arr = new int[5];
+arr[0] = 5;
+arr[1] = 7;
+arr[2] = 9;
+arr[3] = 2;
+arr[4] = 3;
+Console.WriteLine(arr[0]);
+Console.WriteLine(arr[1]);
+Console.WriteLine(arr[2]);
+Console.WriteLine(arr[3]);
+Console.WriteLine(arr[4]);
+// Console.WriteLine(arr[5]);
+
+int[] arr2 = new int[5] { 1, 2, 5, 3, 2 };
+
+int[] arr3 = {7, 7, 4, 5, 6};
+
+// arr = [1, 2, 3, 4, 5] так в пайтон
