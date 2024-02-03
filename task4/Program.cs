56b5ceac1e121a7cbba98b0c51f3925e71ee24ca
Roman_Knyazev <ram3es92@mail.ru>
2024-02-03 11:23:46 +0300

    добавили новые циклы

---
+++ task4/Program.cs
@@ -0,0 +1,36 @@
+﻿int n = 5;
+int[] arr = {4, 21, 6, 9, 3};
+int i = 0;
+int max = arr[0];
+
+while(i < n)
+{
+    if(arr[i] > max)
+    {
+        max = arr[i];
+    }
+    i = i + 1;
+}
+Console.WriteLine(max);
+
+max = arr[0];
+
+for(int j = 0; j < n; j++)
+{
+    if(arr[j] > max)
+    {
+        max = arr[j];
+    }
+}
+Console.WriteLine(max);
+
+max = arr[0];
+
+foreach(int e in arr)
+{
+        if(e > max)
+    {
+        max = e;
+    }
+}
+Console.WriteLine(max);
